15e28e618e272afdcf3a9a371aababc3383bec69
Iceheop <Iceheop@outlook.com>
2025-02-21 17:01:36 -0400

    Modificado

--- setup.sh
+++ setup.sh
@@ -4,27 +4,18 @@
 apt-get update
 apt-get install -y wget unzip git
 
-# Descargar y configurar Ngrok para el túnel
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O /content/ngrok.zip
-if [ $? -eq 0 ]; then
-  unzip /content/ngrok.zip -d /content
-  /content/ngrok authtoken 2tMhLL2jNo19p4V5BElz3VD4rCY_3Qga6TALLho6TNWSzVkGr
-else
-  echo "Error: No se pudo descargar Ngrok."
-  exit 1
-fi
+# Clonar el repositorio de GitHub
+# git clone https://github.com/tu_usuario/tu_repositorio.git /content/terraria-server
+
+# Configurar Ngrok para el túnel
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok.zip
+unzip ngrok.zip
+./ngrok authtoken cr_2tMKqeDiV4vCIvTXzej5z6mBDvs
 
 # Dar permisos de ejecución a los archivos TerrariaServer y TerrariaServer.bin.x86_64
 chmod +x /content/terraria-server/1436/Linux/TerrariaServer
 chmod +x /content/terraria-server/1436/Linux/TerrariaServer.bin.x86_64
 
-# Iniciar el servidor de Terraria y crear un mundo por defecto
+# Iniciar el servidor de Terraria
 cd /content/terraria-server/1436/Linux
-./TerrariaServer -config /content/terraria-server/1436/Linux/serverconfig.txt &
-
-if [ $? -eq 0 ]; then
-  echo "El servidor de Terraria se ha iniciado correctamente."
-else
-  echo "Error: No se pudo iniciar el servidor de Terraria."
-  exit 1
-fi
+./TerrariaServer -config serverconfig.txt
