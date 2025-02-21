084b74f2d2b4245cbb54a20abbe6e22bba22496c
Iceheop <Iceheop@outlook.com>
2025-02-21 17:40:16 -0400

    Modificado

--- setup.sh
+++ setup.sh
@@ -9,9 +9,6 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok.
 unzip ngrok.zip
 ./ngrok authtoken cr_2tMKqeDiV4vCIvTXzej5z6mBDvs
 
-# Iniciar el túnel Ngrok para el puerto 7777
-./ngrok tcp 7777 &
-
 # Dar permisos de ejecución a los archivos TerrariaServer y TerrariaServer.bin.x86_64
 chmod +x /content/terraria-server/1436/Linux/TerrariaServer
 chmod +x /content/terraria-server/1436/Linux/TerrariaServer.bin.x86_64
@@ -19,6 +16,12 @@ chmod +x /content/terraria-server/1436/Linux/TerrariaServer.bin.x86_64
 # Iniciar el servidor de Terraria
 cd /content/terraria-server/1436/Linux
 ./TerrariaServer -config serverconfig.txt
+
+# Iniciar el túnel Ngrok para el puerto 7777
+./ngrok tcp 7777 &
+
+# Esperar unos segundos para que Ngrok establezca el túnel
+sleep 5 
 
 # Mostrar el estado del túnel Ngrok
 curl -s http://localhost:4040/api/tunnels | grep -o "tcp://0.tcp.ngrok.io:[0-9]*"
