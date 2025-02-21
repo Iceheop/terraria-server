8eda98b0ac8ea910b37b13c779401b9e2227eb79
Iceheop <Iceheop@outlook.com>
2025-02-21 15:21:03 -0400

    Modificado

--- setup.sh
+++ setup.sh
@@ -8,9 +8,9 @@ apt-get install -y wget unzip git
 git clone https://github.com/tu_usuario/tu_repositorio.git /content/terraria-server
 
 # Descargar y configurar Ngrok para el túnel
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O /content/terraria-server/ngrok.zip
-unzip /content/terraria-server/ngrok.zip -d /content/terraria-server
-/content/terraria-server/ngrok authtoken cr_2tMKqeDiV4vCIvTXzej5z6mBDvs
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O /content/ngrok.zip
+unzip /content/ngrok.zip -d /content
+/content/ngrok authtoken cr_2tMKqeDiV4vCIvTXzej5z6mBDvs
 
 # Descargar el mundo Venesuela_chamo.wld
 wget https://github.com/Iceheop/terraria-server/raw/main/1436/Linux/Terraria/Worlds/Venesuela_chamo.wld -O /content/terraria-server/1436/Linux/Terraria/Worlds/Venesuela_chamo.wld
@@ -29,10 +29,10 @@ chmod +x /content/terraria-server/1436/Linux/TerrariaServer.bin.x86_64
 
 # Iniciar el servidor de Terraria con el mundo descargado
 cd /content/terraria-server/1436/Linux
-./TerrariaServer -world /content/terraria-server/1436/Linux/Terraria/Worlds/Venesuela_chamo.wld -config serverconfig.txt &
+./TerrariaServer -world /content/terraria-server/1436/Linux/Terraria/Worlds/Venesuela_chamo.wld &
 
 # Exponer el puerto del servidor con Ngrok y obtener la URL pública
-/content/terraria-server/ngrok tcp 7777 &
+/content/ngrok tcp 7777 &
 
 # Esperar unos segundos para que Ngrok se inicie y obtener la URL pública
 sleep 5
