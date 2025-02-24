534309602ed0ea638aef92dac6a7345879fc1a87
Iceheop <Iceheop@outlook.com>
2025-02-24 08:49:31 -0400

    Cambio de version

--- setup.sh
+++ setup.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
 # Dar permisos de ejecución a los archivos TerrariaServer y TerrariaServer.bin.x86_64
-chmod +x /content/terraria-server/1436/Linux/TerrariaServer
-chmod +x /content/terraria-server/1436/Linux/TerrariaServer.bin.x86_64
+chmod +x /content/terraria-server/1449/Linux/TerrariaServer
+chmod +x /content/terraria-server/1449/Linux/TerrariaServer.bin.x86_64
 
 # Verificar la ubicación de los archivos de configuración del servidor
-echo "Archivos en el directorio /content/terraria-server/1436/Linux/:"
-ls -l /content/terraria-server/1436/Linux/
+echo "Archivos en el directorio /content/terraria-server/1449/Linux/:"
+ls -l /content/terraria-server/1449/Linux/
 
 # Iniciar el servidor de Terraria
-echo "Iniciando el servidor de Terraria..."
-cd /content/terraria-server/1436/Linux
-./TerrariaServer -port 7777 -autocreate 2 -secure &
-
-# Mensaje final
-echo "Proceso completado."
+cd /content/terraria-server/1449/Linux
+./TerrariaServer  -config serverconfig.txt
